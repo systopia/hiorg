aef2c7ac9b8730f4331ddc531f2d0a6d621b903f
Jens Schuppe <schuppe@systopia.de>
2023-05-16 15:32:37 +0200

    alter API spec and afform

--- Civi/Hiorg/ConfigProfile.php
+++ Civi/Hiorg/ConfigProfile.php
@@ -11,25 +11,32 @@ use CRM_Hiorg_ExtensionUtil as E;
 
 class ConfigProfile extends \CRM_ConfigProfiles_BAO_ConfigProfile implements ConfigProfileInterface {
 
-  public static function dataFieldSpec(): ?array {
-    return [
-      'xcm_profile' => [
-        'type' => 'Text',
-        'title' => E::ts('Extended Contact Manager (XCM) Profile'),
-        'description' => E::ts('XCM profile to use for processing contacts with this configuration profile.'),
-        'required' => TRUE,
-      ],
-      'oauth_client_id' => [
-        'type' => 'Text',
-        'title' => E::ts('OAuth Client ID'),
-        'description' => E::ts('CiviCRM OAuth Client ID to use for authenticating with this configuration profile.'),
-        'required' => TRUE,
-      ],
-    ];
-  }
+  public static function modifyFieldSpec(RequestSpec $spec): void {
+    $field = new FieldSpec('xcm_profile', 'ConfigProfile', 'String');
+    $field->setTitle(ts('Extended Contact manager (XCM) Profile'));
+    $field->setLabel(ts('XCM Profile'));
+    $field->setDescription(ts('XCM profile to use for processing contacts with this configuration profile.'));
+    $field->setRequired(TRUE);
+    $field->setInputType('Select');
+    $field->setOptions(\CRM_Xcm_Configuration::getProfileList());
+    $spec->addFieldSpec($field);
 
-  public static function afformFields(): ?array {
-
+    $field = new FieldSpec('oauth_client_id', 'ConfigProfile', 'String');
+    $field->setTitle(ts('OAuth Client ID'));
+    $field->setLabel(ts('OAuth Client ID'));
+    $field->setDescription(ts('CiviCRM OAuth Client ID to use for authenticating with this configuration profile.'));
+    $field->setRequired(TRUE);
+    $field->setInputType('Select');
+    $oauth_clients = \Civi\Api4\OAuthClient::get()
+      ->addSelect('guid', 'provider:label')
+      ->execute()
+      ->indexBy('guid')
+      ->getArrayCopy();
+    array_walk($oauth_clients, function(&$oauth_client) {
+      $oauth_client = $oauth_client['guid'] . ' (' . $oauth_client['provider:label'] . ')';
+    });
+    $field->setOptions($oauth_clients);
+    $spec->addFieldSpec($field);
   }
 
   /**
