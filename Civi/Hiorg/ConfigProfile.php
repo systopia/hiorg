e36804c79dba7cbe158b8b0fd871d68e6987c59a
Jens Schuppe <schuppe@systopia.de>
2023-05-19 16:59:07 +0200

    Adjust for new config-profiles structure

--- Civi/Hiorg/ConfigProfile.php
+++ Civi/Hiorg/ConfigProfile.php
@@ -13,40 +13,47 @@ class ConfigProfile extends \CRM_ConfigProfiles_BAO_ConfigProfile implements Con
 
   const NAME = 'hiorg';
 
-  public static function getMetadata(): array {
-    return [
+  public static function getMetadata(bool $includeFields = FALSE): array {
+    $metadata = [
       'name' => self::NAME,
       'label' => E::ts('HiOrg-Server API'),
-      'description' => E::ts('Configuration profiles for the HiOrg-Server API extension.')
+      'description' => E::ts('Configuration profiles for the HiOrg-Server API extension.'),
     ];
+
+    if ($includeFields) {
+      $oauth_clients = \Civi\Api4\OAuthClient::get(FALSE)
+        ->addSelect('guid', 'provider:label')
+        ->execute()
+        ->indexBy('guid')
+        ->getArrayCopy();
+      array_walk($oauth_clients, function (&$oauth_client) {
+        $oauth_client = $oauth_client['guid'] . ' (' . $oauth_client['provider:label'] . ')';
+      });
+      $metadata['fields'] = [
+        'xcm_profile' => (new FieldSpec('xcm_profile', 'ConfigProfile_' . self::NAME, 'String'))
+          ->setTitle(ts('Extended Contact manager (XCM) Profile'))
+          ->setLabel(ts('XCM Profile'))
+          ->setDescription(ts('XCM profile to use for processing contacts with this configuration profile.'))
+          ->setRequired(TRUE)
+          ->setInputType('Select')
+          ->setOptions(\CRM_Xcm_Configuration::getProfileList()),
+        'oauth_client_id' => (new FieldSpec('oauth_client_id', 'ConfigProfile_' . self::NAME, 'String'))
+          ->setTitle(ts('OAuth Client ID'))
+          ->setLabel(ts('OAuth Client ID'))
+          ->setDescription(ts('CiviCRM OAuth Client ID to use for authenticating with this configuration profile.'))
+          ->setRequired(TRUE)
+          ->setInputType('Select')
+          ->setOptions($oauth_clients),
+      ];
+    }
+
+    return $metadata;
   }
 
   public static function modifyFieldSpec(RequestSpec $spec): void {
-    $field = new FieldSpec('xcm_profile', 'ConfigProfile', 'String');
-    $field->setTitle(ts('Extended Contact manager (XCM) Profile'));
-    $field->setLabel(ts('XCM Profile'));
-    $field->setDescription(ts('XCM profile to use for processing contacts with this configuration profile.'));
-    $field->setRequired(TRUE);
-    $field->setInputType('Select');
-    $field->setOptions(\CRM_Xcm_Configuration::getProfileList());
-    $spec->addFieldSpec($field);
+  }
 
-    $field = new FieldSpec('oauth_client_id', 'ConfigProfile', 'String');
-    $field->setTitle(ts('OAuth Client ID'));
-    $field->setLabel(ts('OAuth Client ID'));
-    $field->setDescription(ts('CiviCRM OAuth Client ID to use for authenticating with this configuration profile.'));
-    $field->setRequired(TRUE);
-    $field->setInputType('Select');
-    $oauth_clients = \Civi\Api4\OAuthClient::get()
-      ->addSelect('guid', 'provider:label')
-      ->execute()
-      ->indexBy('guid')
-      ->getArrayCopy();
-    array_walk($oauth_clients, function(&$oauth_client) {
-      $oauth_client = $oauth_client['guid'] . ' (' . $oauth_client['provider:label'] . ')';
-    });
-    $field->setOptions($oauth_clients);
-    $spec->addFieldSpec($field);
+  public static function processValues(array $item, array &$data): void {
   }
 
   /**
