c447c25a7e283b1c7f58553145a9e1a607729c0a
Jens Schuppe <schuppe@systopia.de>
2024-02-05 14:10:52 +0100

    Provide scheduled jobs for synchronizing

---
+++ managed/Job__hiorg_synchronize_contacts.mgd.php
@@ -0,0 +1,38 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA HiOrg-Server API                              |
+| Copyright (C) 2024 SYSTOPIA                            |
+| Author: J. Schuppe (schuppe@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Hiorg_ExtensionUtil as E;
+
+return [
+  [
+    'name' => 'Job__hiorg_synchronize_contacts',
+    'entity' => 'Job',
+    'cleanup' => 'always',
+    'update' => 'never',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'run_frequency:name' => 'Always',
+        'name' => E::ts('HiOrg-Server API: Synchronize Contacts'),
+        'description' => E::ts('Fetches HiOrg-Server users and related records via the HiOrg-Server API and synchronizes them with CiviCRM contacts.'),
+        'api_entity' => 'Hiorg',
+        'api_action' => 'synchronizeContacts',
+        'parameters' => 'version=4',
+        'is_active' => FALSE,
+      ],
+      'match' => [],
+    ],
+  ],
+];
---
+++ managed/Job__hiorg_synchronize_volunteer_hours.mgd.php
@@ -0,0 +1,38 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA HiOrg-Server API                              |
+| Copyright (C) 2024 SYSTOPIA                            |
+| Author: J. Schuppe (schuppe@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Hiorg_ExtensionUtil as E;
+
+return [
+  [
+    'name' => 'Job__hiorg_synchronize_volunteer_hours',
+    'entity' => 'Job',
+    'cleanup' => 'always',
+    'update' => 'never',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'run_frequency:name' => 'Always',
+        'name' => E::ts('HiOrg-Server API: Synchronize Volunteer Hours'),
+        'description' => E::ts('Fetches HiOrg-Server volunteer hours via the HiOrg-Server API and synchronizes them with CiviCRM activities.'),
+        'api_entity' => 'Hiorg',
+        'api_action' => 'synchronizeVolunteerHours',
+        'parameters' => 'version=4',
+        'is_active' => FALSE,
+      ],
+      'match' => [],
+    ],
+  ],
+];
